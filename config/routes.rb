95afd6832d033b9be19c364e7ef8855b91fdb80d
prochampion1 <thinhduong812@gmail.com>
2016-11-08 19:01:48 +0700

    facebook google update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
- 
   root 'home#index'
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   resource :recipes
+  devise_for :users, class_name: 'User',
+    :controllers => {:omniauth_callbacks => 'users/omniauth_callbacks', registrations: 'users/registrations'}
+  resource :recipes
+
+
 end
+

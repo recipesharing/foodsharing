56119cf0668042e808d2d199136fc1369c81d4b6
Vu Toan <vutoan@Vus-MacBook-Pro.lan>
2016-11-12 19:05:20 +0700

    improve style for recipe show a little

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112092043) do
+ActiveRecord::Schema.define(version: 20161112113629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20161112092043) do
     t.integer  "step"
     t.string   "title"
     t.text     "content"
+    t.string   "image_url"
     t.index ["recipe_id"], name: "index_instructions_on_recipe_id", using: :btree
   end
 
@@ -109,6 +110,7 @@ ActiveRecord::Schema.define(version: 20161112092043) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 

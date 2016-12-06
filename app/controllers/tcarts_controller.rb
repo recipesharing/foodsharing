714acd73f4b32a9233d8a0eea12faeed6f5822b0
Vu Toan <vutoan@Vus-MacBook-Pro.lan>
2016-12-06 13:34:04 +0700

    update checkout page

--- app/controllers/tcarts_controller.rb
+++ app/controllers/tcarts_controller.rb
@@ -1,32 +1,28 @@
 class TcartsController < ApplicationController
   before_action :authenticate_user!
+  before_action :prepare_user, only: [:index, :show, :checkout]
   
   def index
     @torder_items = current_torder.torder_items
-    @user = current_user
+    # @user = current_user
   end 
 
   def show
     @torder_items = current_torder.torder_items
   end
 
-  def update
-    raise 'e'
-  end
-
-
   def checkout
-    @user = current_user
+    # @user = current_user
     @torder = Torder.new
     @torder_items = current_torder.torder_items
-      if current_user.nil?
-        flash[:warning]="please sign in "
-      else 
-        @torder_items = current_torder.torder_items
-        @total_price_each_item = @torder_items.map(&:total_price)
-        sum = 0 
-        @total_all  = @total_price_each_item.inject(0){|sum,x| sum + x } 
-      end
+    if current_user.nil?
+      flash[:warning]="please sign in "
+    else 
+      @torder_items = current_torder.torder_items
+      @total_price_each_item = @torder_items.map(&:total_price)
+      sum = 0 
+      @total_all  = @total_price_each_item.inject(0){|sum,x| sum + x } 
+    end
   end
 
   def create
@@ -45,8 +41,12 @@ class TcartsController < ApplicationController
     end
 
     def torder_item_params
-    params.require(:torder_item).permit( :quantity, :recipe_id)
-  end
+      params.require(:torder_item).permit( :quantity, :recipe_id)
+    end
+
+    def prepare_user
+      @user = current_user if user_signed_in?
+    end
 end
 
 
